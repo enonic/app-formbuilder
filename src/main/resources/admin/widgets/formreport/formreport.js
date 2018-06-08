5d8471d23a8212b5ab37ecf29ba1aa3430df7910
Bjørnar Hjelmevold <bhj@enonic.com>
2018-06-08 05:28:21 +0200

    date handling in report

--- src/main/resources/admin/widgets/formreport/formreport.js
+++ src/main/resources/admin/widgets/formreport/formreport.js
@@ -2,49 +2,117 @@ var contentLib = require('/lib/xp/content');
 var portalLib = require('/lib/xp/portal');
 var thymeleaf = require('/lib/xp/thymeleaf');
 var ioLib = require('/lib/xp/io');
+var nodeLib = require('/lib/xp/node');
+var util = require('/lib/enonic/util/data');
+var moment = require('/lib/moment.min.js');
 
 var view = resolve('formreport.html');
+
 /*var cssFile = ioLib.getResource(('/assets/css/formreport.css'));
 var css = ioLib.readText(cssFile.getStream());*/
 
 function handleGet(req) {
     var uid = req.params.uid;
-    /*log.info('uid');
-    log.info(JSON.stringify(uid, null, 4));*/
     var contentId = req.params.contentId;
-    /*log.info('contentId');
-    log.info(JSON.stringify(contentId, null, 4));*/
     if (!contentId) {
         contentId = portalLib.getContent()._id;
     }
 
-    var form = contentLib.get({
+    var content = contentLib.get({
         key: contentId,
         branch: 'draft'
     });
+    var isForm = (content.type === app.name + ':form');
 
-    var responses = contentLib.query({
-        branch: 'master',
-        query: '',
-        contentTypes: ['base:unstructured']
-    });
-
-    var hasNoResponses = !(responses.hits.length);
-
-    var isForm = (form.type === app.name + ':form');
-
-    var actionUrl = portalLib.serviceUrl({ service: 'formreport' });
-
-    var params = {
+    var model = {
         uid: uid,
+        content: content,
         isForm: isForm,
-        actionUrl: actionUrl,
-        filename: form.displayName + '.csv'
+        actionUrl: portalLib.serviceUrl({ service: 'formreport' }),
+        currentDate: moment().format('YYYY-MM-DD'),
+        filename: content.displayName + '-' + moment().format('YYYY-MM-DD') + '.csv'
     };
+
+    if (isForm) {
+        var contentPath = content._path;
+        var sitePath = contentPath.split('/')[1];
+
+        var site = contentLib.get({
+            key: '/' + sitePath,
+            branch: 'draft'
+        });
+
+        var siteConfig = null;
+        if (site && site.data && site.data.siteConfig) {
+            // Traverse siteConfigs for one or all apps of a site, returning the config for the last match of this app (should only be one match anyway)
+            // This is basically just the same as portalLib.getSiteConfig() if the current site was already in the context
+            util.forceArray(site.data.siteConfig).forEach(function (sc, index) {
+                if (sc.applicationKey === app.name) {
+                    siteConfig = sc.config;
+                }
+            });
+        }
+
+        var formbuilderRepo = nodeLib.connect({
+            repoId: (siteConfig.storageLocation === 'cmsRepo') ? 'cms-repo' : 'com.enonic.formbuilder',
+            branch: 'draft',
+            principals: ['role:system.admin']
+        });
+
+        var responses = formbuilderRepo.query({
+            branch: 'draft',
+            query: 'data._formContentId = "' + contentId + '"',
+            sort: 'createdTime ASC',
+            count: 1,
+            contentTypes: ['base:unstructured'],
+            aggregations: {
+                oldestCreatedTime: {
+                    terms: {
+                        field: 'createdTime',
+                        order: 'count DESC',
+                        size: 1
+                    }
+                }
+            }
+        });
+
+        var lastResponse = formbuilderRepo.query({
+            branch: 'draft',
+            query: 'data._formContentId = "' + contentId + '"',
+            sort: 'createdTime DESC',
+            count: 1,
+            contentTypes: ['base:unstructured'],
+            aggregations: {
+                newestCreatedTime: {
+                    terms: {
+                        field: 'createdTime',
+                        order: 'count DESC',
+                        size: 1
+                    }
+                }
+            }
+        });
+
+        var hasResponses = (responses.total > 0);
+
+        model.hasResponses = hasResponses;
+        model.numResponses = responses.total;
+        model.oldestCreatedDate = (responses.aggregations && responses.aggregations.oldestCreatedTime
+            && responses.aggregations.oldestCreatedTime.buckets
+            && responses.aggregations.oldestCreatedTime.buckets[0]
+            && responses.aggregations.oldestCreatedTime.buckets[0].key) ?
+                responses.aggregations.oldestCreatedTime.buckets[0].key.substring(0, 10) : null;
+        model.newestCreatedDate = (lastResponse.aggregations && lastResponse.aggregations.newestCreatedTime
+            && lastResponse.aggregations.newestCreatedTime.buckets
+            && lastResponse.aggregations.newestCreatedTime.buckets[0]
+            && lastResponse.aggregations.newestCreatedTime.buckets[0].key) ?
+                lastResponse.aggregations.newestCreatedTime.buckets[0].key.substring(0, 10) : null;
+        model.repoId = (siteConfig.storageLocation === 'cmsRepo') ? 'cms-repo' : 'com.enonic.formbuilder';
+    }
 
     return {
         contentType: 'text/html',
-        body: thymeleaf.render(view, params)
+        body: thymeleaf.render(view, model)
     };
 }
 exports.get = handleGet;
--- src/main/resources/services/formreport/formreport.js
+++ src/main/resources/services/formreport/formreport.js
@@ -1,42 +1,108 @@
 var contentLib = require('/lib/xp/content');
 var portalLib = require('/lib/xp/portal');
+var nodeLib = require('/lib/xp/node');
 var util = require('/lib/enonic/util/data');
+var moment = require('/lib/moment.min.js');
+
+function createCSV(responses, formContent, separator) {
+    log.info('responses');
+    log.info(JSON.stringify(responses, null, 4));
+    log.info('formContent');
+    log.info(JSON.stringify(formContent, null, 4));
+    log.info('separator');
+    log.info(JSON.stringify(separator, null, 4));
+    
+    // TODO: improve fields array, at least base it on the most recent content or on form content id from URL parameter
+    // TODO: get fields from form content!
+    var fields = Object.keys(responses[0].data);
+
+    var responseData = [];
+    responses.forEach(function (hit) {
+        responseData.push(hit.data);
+    });
+
+    var replacer = function(key, value) {
+        return (value === null) ? '' : value;
+    };
+
+    var csv = responseData.map(function(row) {
+        return fields.map(function(fieldName) {
+            return JSON.stringify(row[fieldName], replacer);
+        }).join(separator);
+    });
+
+    // add header column
+    csv.unshift(fields.join(separator));
+
+    return csv.join('\r\n');
+}
 
 function handleGet(req) {
-    /*log.info('formreport service req');
-    log.info(JSON.stringify(req, null, 4));*/
+    var separator = (req.params.separator && req.params.separator.trim()) ? req.params.separator.trim() : ';';
+    var filename = (req.params.filename && req.params.filename.trim()) ? req.params.filename.trim() : 'formbuilder-report.csv';
+    var repoId = (req.params.repoId && req.params.repoId.trim()) ? req.params.repoId.trim() : 'com.enonic.formbuilder';
+    var fromDate = (req.params.fromDate && req.params.fromDate.trim()) ? req.params.fromDate.trim() : null;
+    var toDate = (req.params.toDate && req.params.toDate.trim()) ? req.params.toDate.trim() : null;
+    var purge = (req.params.purge) ? true : false;
+    var formContentId = (req.params.formContentId && req.params.formContentId.trim()) ? req.params.formContentId.trim() : null;
 
-    var separator = (req.params.separator && req.params.separator.trim()) ? req.params.separator.trim() : ',';
+    if (!formContentId) {
+        throw new Error('Missing form content ID.');
+    }
 
-    var responses = contentLib.query({
-        branch: 'master',
-        query: '',
+    var formContentInMaster = contentLib.get({
+        key: formContentId,
+        branch: 'master'
+    });
+    var formContentInDraft = contentLib.get({
+        key: formContentId,
+        branch: 'draft'
+    });
+
+    var formContent = formContentInMaster || formContentInDraft;
+
+    if (!formContent) {
+        throw new Error('Could not find any form content by the specified ID.');
+    }
+
+    // TODO: test what happens with invalid repo name, throw error
+    var formbuilderRepo = nodeLib.connect({
+        repoId: repoId,
+        branch: 'draft',
+        principals: ['role:system.admin']
+    });
+
+    var responseKeysQuery = 'data._formContentId = "' + formContentId + '"';
+    if (fromDate || toDate) {
+        var fromDateTime = (fromDate && moment(fromDate)) ? moment(fromDate).format('YYYY-MM-DD') + 'T00:00:00Z' : '1970-01-01T00:00:00Z';
+        // add one day to include hits from last day specified, fallback to tomorrow
+        var toDateTime = (toDate && moment(toDate)) ? moment(toDate).add(1, 'days').format('YYYY-MM-DD') + 'T00:00:00Z' : moment().add(1, 'days').format('YYYY-MM-DD') + 'T00:00:00Z';
+        responseKeysQuery += ' AND range("createdTime", instant("' + fromDateTime + '"), instant("' + toDateTime + '"), "true", "false")';
+    }
+
+    var responsesMetadata = formbuilderRepo.query({
+        branch: 'draft',
+        query: responseKeysQuery,
+        sort: 'createdTime ASC',
+        count: -1,
         contentTypes: ['base:unstructured']
     });
 
     var csv = '';
-    var fields = Object.keys(responses.hits[0].data);
-    /*log.info('fields');
-    log.info(JSON.stringify(fields, null, 4));*/
-    var responseData = [];
-    responses.hits.forEach(function (hit) {
-        responseData.push(hit.data);
-    });
-    /*log.info('responseData');
-    log.info(JSON.stringify(responseData, null, 4));*/
+    if (responsesMetadata.hits.length) {
+        var responses = [];
+        responsesMetadata.hits.forEach(function (hit) {
+            responses.push(formbuilderRepo.get(hit.id));
+        });
+        csv = createCSV(responses, formContent, separator);
+        log.info('csv');
+        log.info(JSON.stringify(csv, null, 4));
+    }
 
-    var replacer = function(key, value) { return value === null ? '' : value };
-
-    csv = responseData.map(function(row){
-        return fields.map(function(fieldName){
-            return JSON.stringify(row[fieldName], replacer)
-        }).join(separator);
-    });
-    // add header column
-    csv.unshift(fields.join(separator));
+    return false;
 
     return {
-        body: csv.join('\r\n'),
+        body: '',
         contentType: 'text/csv',
         contentDisposition: 'attachment; filename="' + req.params.filename + '"'
     };
